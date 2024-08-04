58665f4d727e016c3e0cbc1f6fafc905cdd42a64
graceK-10 <kipagrace@gmail.com>
2024-08-04 09:51:04 +0200

    removed about added form logic

---
+++ src/pages/Post.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Header";
+import Form from "../components/Form";
+
+function Post() {
+  return (
+    <div>
+      <Navbar />
+      <Form />
+    </div>
+  );
+}
+
+export default Post;
