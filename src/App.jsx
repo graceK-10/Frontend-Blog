346c1b71b7c73a3ef7369b83df280c1d8d47a50b
graceK-10 <kipagrace@gmail.com>
2024-08-04 09:43:45 +0200

    created app.jsx page imported home, post, and detailedblog page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+import Detailedblog from "./components/Detailedblog";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new-blog" element={<Post />} />
+          <Route path="/blog/:id" element={<Detailedblog />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   );
-}
+};
 
 export default App;
