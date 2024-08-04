25c18c7da1c77d35b79d6d3a92a8b1f6d613b1e8
graceK-10 <kipagrace@gmail.com>
2024-08-04 09:48:20 +0200

    added navbar and main components into my home.jsx page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Header";
+import Main from "../components/Main";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default Home;
