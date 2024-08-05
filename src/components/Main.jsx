4da1ac694f1752be9c33682fdefcf46402d0e0b2
graceK-10 <kipagrace@gmail.com>
2024-08-05 18:16:06 +0200

    commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -29,7 +29,22 @@ const Main = () => {
     fetchPosts();
   }, []);
 
-  return <section></section>;
+  return (
+    <section className="relative py-20 bg-orange-50 overflow-hidden min-h-screen flex flex-col">
+      <div className="relative container px-4 mx-auto h-full flex flex-col justify-center">
+        <div className="max-w-xl lg:max-w-7xl mx-auto">
+          <div className="max-w-2xl mx-auto mb-15 text-center lg:mb-9">
+            <h1 className="font-heading text-5xl xs:text-2xl md:text-7xl font-bold">
+              <span className="custom-break pr-4 font-serif italic">
+                Cooking
+              </span>
+              <span className="custom-break font-serif italic">Chronicles</span>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Main;
